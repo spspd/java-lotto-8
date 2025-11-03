a2d7b2682efe22bbeac574775c99b767a9d815ec
spspd <zip02934@hanyang.ac.kr>
2025-11-03 18:06:56 +0900

    feat : buy lotto

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,9 +5,8 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        int amount = lotto.method.buy.buyLotto();
+        int amount = lotto.method.Buy.buyLotto();
         System.out.println(amount);
-        Lotto a = new lotto.Lotto(List.of(1,2,3,4,5,6));
 
     }
 }
--- src/main/java/lotto/method/buy.java
+++ src/main/java/lotto/method/buy.java
@@ -1,14 +1,43 @@
 package lotto.method;
 
-public class buy {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Buy {
+    public static List<List<Integer>> list_lotto = new ArrayList<>(); // public 할필요는 없다.
+
     public static int buyLotto(){
+        // TODO : 길이 넘음 - 각 함수 호출만 으로 분리필요
         System.out.println("구입금액을 입력해 주세요.");
-        int money = Integer.parseInt(camp.nextstep.edu.missionutils.Console.readLine());
+        int money = 0;
+
+        try{
+            money = Integer.parseInt(camp.nextstep.edu.missionutils.Console.readLine());
+        }
+        // TODO : 오류
+        catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 입력 오류(나중에 바꿈)"); 
+        }
         int amount = amountLotto(money);
+        System.out.println(amount + "개를 구매했습니다.");
+        for(int i = 0; i < amount; i++){
+            List<Integer> paid_Lotto = numbLotto();
+            paid_Lotto.sort(null);
+            list_lotto.add(paid_Lotto);
+        }
+        
         return amount;
     }
     public static int amountLotto(int money){
         int amount = money / 1000;
         return amount;
     }
+
+    public static List<Integer> numbLotto(){
+        List<Integer> paid_Lotto = camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        System.out.println(paid_Lotto);
+        return paid_Lotto;
+    }
+
+
 }
