9c9dee520e0791814deb11f8465824ee566cc556
spspd <zip02934@hanyang.ac.kr>
2025-11-03 20:37:45 +0900

    update : 누락된 할일 갱신

--- src/main/java/lotto/method/Setting.java
+++ src/main/java/lotto/method/Setting.java
@@ -3,6 +3,7 @@ package lotto.method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
+import java.util.HashSet;
 
 import lotto.Lotto;
 public class Setting {
@@ -19,19 +20,31 @@ public class Setting {
         //TODO
         String a = camp.nextstep.edu.missionutils.Console.readLine();
         String[] numbers ;
+        List<Integer> numbers_int = new ArrayList<>();
+
         try{
             numbers = a.split(",");
+            for (int i = 0; i < numbers.length; i++) {
+                numbers_int.add(Integer.parseInt(numbers[i]));
+            }   
+            validate_winNumber(numbers_int); 
         } 
         catch (NumberFormatException e){
-            throw new IllegalArgumentException("[ERROR] 입력 오류(나중에 바꿈)"); 
+            throw new IllegalArgumentException("[ERROR] 로또 번호 입력 오류"); 
         }
-        List<Integer> numbers_int = new ArrayList<>();
-        // validate_winNumber(); trycatch로 대체
-        for (int i = 0; i < numbers.length; i++) {
-            numbers_int.add(Integer.parseInt(numbers[i]));
-        }   
+        
+        
         Collections.sort(numbers_int);
         winLotto = Lotto.makeLotto(numbers_int);
+    }
+    public static void validate_winNumber(List<Integer> numbers){
+        if(numbers.size() != 6){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+        if(numbers.size() != new HashSet<Integer>(numbers).size()){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+        }
+
     }
 
     public void bonusNumber(){
